35a777dfb600d9c3e9882af56e8858ff368e4800
sunnyGupta128 <sunnygupta6497@gmail.com>
2025-06-05 19:41:37 +0530

    feat: implement user authentication check in header component; add checkUser function for user validation and create necessary database migrations

--- components/header.jsx
+++ components/header.jsx
@@ -1,27 +1,31 @@
+
+
 import React from 'react'
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
-// import { Header } from '@radix-ui/react-accordion'
 import Link from 'next/link'
 import Image from 'next/image'
 import { Button } from './ui/button'
 import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarIcon } from 'lucide-react'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu'
+import { checkUser } from '@/lib/clerkUser'
 
 
-function header() {
+const Header = async () => {
+  // await checkUser();
+  await checkUser(); // Ensure user is checked on header load
   return (
     <header className='fixed top-0 w-full z-50 border-b bg-background/80 backdrop-blur-md supports-[backdrop-f]:bg-background/80'>
       <nav className='container mx-auto px-4 pt-2 flex items-center justify-between'>
         <Link href="/" className='flex items-center space-x-2'>
           <Image src="/logo.png" alt="Logo" width={100} height={50}
-           className='h-20  w-auto object-contain'
+            className='h-20  w-auto object-contain'
           />
         {/* <p>ABC</p> */}
         </Link>
 
         <div className='flex items-center space-x-4 md:space-x-4'>
           <SignedIn>
-            <Link href={"/dashboard"}>
+            <Link href={"/onboarding"}>
               <Button variant="outline" >
                 <LayoutDashboard className='mr-2 h-4 w-4' />
                 <span className='hidden md:block'>Dashboard</span>
@@ -86,4 +90,4 @@ function header() {
   )
 }
 
-export default header
+export default Header
