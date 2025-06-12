03eccb47eed24fcb6ad47b872a3cb98ceafbfedb
sunnyGupta128 <sunnygupta6497@gmail.com>
2025-06-13 02:59:29 +0530

    feat: add Quiz component and integrate it into MockInterviewPage

---
+++ app/(main)/interview/_components/quiz.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Quiz = props => {
+  return (
+    <div>quiz</div>
+  )
+}
+
+
+
+export default Quiz
--- app/(main)/interview/mock/page.jsx
+++ app/(main)/interview/mock/page.jsx
@@ -1,8 +1,29 @@
+import { Button } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react'
 
 function MockInterviewPage() {
   return (
-    <div>MockInterviewPage</div>
+    <div>
+      <div className='flex flex-col space-x-2 px-2'>
+        <Link href={"/interview"}>
+          <Button variant="link" className="gap-2 pl-0">
+            <ArrowLeft className='w-4 h-4' />
+            Back to Interview Preparations...
+          </Button>
+        </Link>
+
+        <div className='flex flex-col space-y-2'>
+          <h2 className='text-4xl font-semibold gradient-title'>Mock Interview</h2>
+          <p className='text-muted-foreground'>
+            Get ready for your interview with our mock interview service.
+          </p>
+        </div>
+      </div>
+
+      <Quiz />
+    </div>
   )
 }
 
